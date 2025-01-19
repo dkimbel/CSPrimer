e094acaad7e93e824daf061f5206f282bd8c21c9
Daniel Kimbel <dk@danielkimbel.com>
2025-01-18 21:16:39 -0500

    Implement warnsdorf heuristic for at least 60x speedup in 8x8 search

--- algorithms/trees-and-graphs/knights-tour/src/main.rs
+++ algorithms/trees-and-graphs/knights-tour/src/main.rs
@@ -11,7 +11,7 @@ impl BoardState {
         BoardState { visited, path }
     }
 
-    fn clone_with_added_coords(&self, x: usize, y: usize) -> BoardState {
+    fn clone_with_visited_coords(&self, x: usize, y: usize) -> BoardState {
         let mut new_board_state = self.clone();
         new_board_state.visited[y][x] = true;
         new_board_state.path.push((x, y));
@@ -34,6 +34,7 @@ fn find_tour_dfs(
     let starting_coords = (0, 0);
     let total_num_board_spaces = x_dimension * y_dimension;
     let mut stack = vec![(starting_coords, BoardState::new(x_dimension, y_dimension))];
+    let mut next_options = Vec::with_capacity(8);
 
     while let Some(((x, y), board_state)) = stack.pop() {
         if board_state.path.len() == total_num_board_spaces
@@ -53,31 +54,54 @@ fn find_tour_dfs(
         let can_move_long_left = x >= 2;
         let can_move_short_left = x >= 1;
 
+        next_options.clear();
         if can_move_long_upwards && can_move_short_right {
-            stack.push(((x + 1, y + 2), board_state.clone_with_added_coords(x, y)));
+            next_options.push((x + 1, y + 2));
         }
         if can_move_long_upwards && can_move_short_left {
-            stack.push(((x - 1, y + 2), board_state.clone_with_added_coords(x, y)));
+            next_options.push((x - 1, y + 2));
         }
         if can_move_long_downwards && can_move_short_right {
-            stack.push(((x + 1, y - 2), board_state.clone_with_added_coords(x, y)));
+            next_options.push((x + 1, y - 2));
         }
         if can_move_long_downwards && can_move_short_left {
-            stack.push(((x - 1, y - 2), board_state.clone_with_added_coords(x, y)));
+            next_options.push((x - 1, y - 2));
         }
         if can_move_short_upwards && can_move_long_right {
-            stack.push(((x + 2, y + 1), board_state.clone_with_added_coords(x, y)));
+            next_options.push((x + 2, y + 1));
         }
         if can_move_short_upwards && can_move_long_left {
-            stack.push(((x - 2, y + 1), board_state.clone_with_added_coords(x, y)));
+            next_options.push((x - 2, y + 1));
         }
         if can_move_short_downwards && can_move_long_right {
-            stack.push(((x + 2, y - 1), board_state.clone_with_added_coords(x, y)));
+            next_options.push((x + 2, y - 1));
         }
         if can_move_short_downwards && can_move_long_left {
-            stack.push(((x - 2, y - 1), board_state.clone_with_added_coords(x, y)));
+            next_options.push((x - 2, y - 1));
         }
+
+        // The last thing added to the stack is popped first; we want to pop the lowest
+        // warnsdorf distance first, so we want it added to the stack last. So: sort
+        // by warnsdorf DESCENDING.
+        next_options.sort_by(|(x1, y1), (x2, y2)| {
+            let w1 = warnsdorf_distance(*x1, *y1, x_dimension, y_dimension);
+            let w2 = warnsdorf_distance(*x2, *y2, x_dimension, y_dimension);
+            w2.cmp(&w1)
+        });
+        next_options.iter().for_each(|(new_x, new_y)| {
+            let new_board_state = board_state.clone_with_visited_coords(x, y);
+            stack.push(((*new_x, *new_y), new_board_state));
+        });
     }
 
     None
+}
+
+// Calculate the total distance of a point from both edges of the board (distance from
+// nearest vertical edge plus distance from nearest horizontal edge).
+fn warnsdorf_distance(x: usize, y: usize, x_dimension: usize, y_dimension: usize) -> usize {
+    let max_x = x_dimension - 1;
+    let max_y = y_dimension - 1;
+
+    std::cmp::min(max_x - x, x) + std::cmp::min(max_y - y, y)
 }
