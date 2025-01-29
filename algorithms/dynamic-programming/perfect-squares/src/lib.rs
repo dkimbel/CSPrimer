4befd1c088149d89937be1416d097317675b419d
Daniel Kimbel <dk@danielkimbel.com>
2025-01-28 21:09:07 -0500

    Use the iterator pattern to save allocations of vecs

--- algorithms/dynamic-programming/perfect-squares/src/lib.rs
+++ algorithms/dynamic-programming/perfect-squares/src/lib.rs
@@ -1,23 +1,51 @@
 use std::collections::{HashSet, VecDeque};
 
+struct PerfectSquaresIterator {
+    current_base: usize,
+    max_squared_value: usize,
+}
+
+impl PerfectSquaresIterator {
+    fn new(max_squared_value: usize) -> Self {
+        Self {
+            current_base: 1,
+            max_squared_value,
+        }
+    }
+}
+
+// provide perfect squares, one at a time, until the perfect square
+// would have been larger than the target number
+impl Iterator for PerfectSquaresIterator {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let squared_value = self.current_base.pow(2);
+        if squared_value > self.max_squared_value {
+            return None;
+        }
+
+        self.current_base += 1;
+        Some(squared_value)
+    }
+}
+
 pub fn fewest_perfect_squares_bottom_up(n: usize) -> Vec<usize> {
     // 'Key' to memo is just index into vec. At index 0, we place
     // an empty vec (there are no perfect squares that sum to 0).
     let mut memo: Vec<Vec<usize>> = vec![vec![]];
-    let mut x: usize = 1;
+    let mut target: usize = 1;
 
-    while x <= n {
-        let lesser_or_equal_squares = get_perfect_squares_smaller_or_eq(x);
-        let (new_square, prev_solution) = lesser_or_equal_squares
-            .iter()
-            .map(|new_square| (new_square, &memo[x - *new_square]))
+    while target <= n {
+        let (new_square, prev_solution) = PerfectSquaresIterator::new(target)
+            .map(|new_square| (new_square, &memo[target - new_square]))
             // take shortest list (list containing fewest perfect squares)
             .min_by(|(_, l1), (_, l2)| l1.len().cmp(&l2.len()))
             .unwrap();
         let mut new_solution = prev_solution.clone();
-        new_solution.push(*new_square);
-        memo.insert(x, new_solution);
-        x += 1;
+        new_solution.push(new_square);
+        memo.insert(target, new_solution);
+        target += 1;
     }
 
     memo[n].clone()
@@ -33,18 +61,16 @@ pub fn fewest_perfect_squares_top_down(n: usize) -> Vec<usize> {
             return memoized.clone();
         }
 
-        let lesser_or_equal_squares = get_perfect_squares_smaller_or_eq(n);
-        let (new_square, smaller_solution) = lesser_or_equal_squares
-            .iter()
+        let (new_square, smaller_solution) = PerfectSquaresIterator::new(n)
             .map(|new_square| {
-                let smaller_solution = inner(n - *new_square, memo);
+                let smaller_solution = inner(n - new_square, memo);
                 (new_square, smaller_solution)
             })
             .min_by(|(_, l1), (_, l2)| l1.len().cmp(&l2.len()))
             .unwrap();
 
         let mut new_solution = smaller_solution.clone();
-        new_solution.push(*new_square);
+        new_solution.push(new_square);
         memo.insert(n, new_solution.clone());
         new_solution
     }
@@ -61,7 +87,7 @@ pub fn fewest_perfect_squares_bfs(n: usize) -> Vec<usize> {
     let mut visited: HashSet<usize> = HashSet::from([n]);
 
     while let Some((n, path)) = queue.pop_front() {
-        for square in get_perfect_squares_smaller_or_eq(n) {
+        for square in PerfectSquaresIterator::new(n) {
             let new_target = n - square;
             if visited.contains(&new_target) {
                 continue;
@@ -80,23 +106,6 @@ pub fn fewest_perfect_squares_bfs(n: usize) -> Vec<usize> {
     }
 
     panic!("Failed to find perfect squares for {n}");
-}
-
-fn get_perfect_squares_smaller_or_eq(n: usize) -> Vec<usize> {
-    let mut perfect_square_base: usize = 1;
-    let mut perfect_squares: Vec<usize> = Vec::new();
-
-    loop {
-        let perfect_square = perfect_square_base.pow(2);
-        if perfect_square > n {
-            break;
-        }
-
-        perfect_squares.push(perfect_square);
-        perfect_square_base += 1;
-    }
-
-    perfect_squares
 }
 
 #[cfg(test)]
