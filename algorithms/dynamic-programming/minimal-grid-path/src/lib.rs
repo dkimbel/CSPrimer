f85fa2c727cb503161da70dd5fe6fcf9722191de
Daniel Kimbel <dk@danielkimbel.com>
2025-01-29 20:46:17 -0500

    Add first pass for bottom-up grid cost, but getting panics

--- algorithms/dynamic-programming/minimal-grid-path/src/lib.rs
+++ algorithms/dynamic-programming/minimal-grid-path/src/lib.rs
@@ -1,6 +1,65 @@
+use std::cmp;
 use std::collections::HashMap;
 
 type Coords = (usize, usize);
+
+pub fn minimal_cost_bottom_up(grid: &[&[u32]]) -> Vec<Coords> {
+    let mut memo: HashMap<Coords, (u32, Vec<Coords>)> = HashMap::new();
+
+    let max_x = grid[0].len() - 1;
+    let max_y = grid.len() - 1;
+    // We plan to evaluate diagonal strips of the grid, one "/"-shaped slice at a time.
+    // Starting at the lower right corner, it will take max_offset iterations to evaluate
+    // the upper left corner. We call the following variables "lower left" because they
+    // form the lower left point of the "/" shape.
+    let mut lower_left_x = max_x;
+    let mut lower_left_y = max_y;
+
+    loop {
+        // When combined with lower_left_x and lower_left_y, 'offset' will give us the
+        // next coords in the "/"-shaped slice of the grid we're currently evaluating.
+        for offset in 0..=cmp::min(max_x - lower_left_x, max_y - lower_left_y) {
+            let (x, y) = (lower_left_x + offset, lower_left_y - offset);
+            let self_cost = grid[y][x];
+
+            let maybe_coords_to_right = if x < max_x { Some((x + 1, y)) } else { None };
+            let maybe_coords_below = if y < max_y { Some((x, y + 1)) } else { None };
+
+            let (min_next_cost, min_next_path) = match (maybe_coords_to_right, maybe_coords_below) {
+                (None, None) => (0, Vec::new()),
+                (Some((right_x, right_y)), Some((down_x, down_y))) => {
+                    let (right_cost, right_path) = memo.get(&(right_x, right_y)).unwrap();
+                    let (down_cost, down_path) = memo.get(&(down_x, down_y)).unwrap();
+                    if right_cost < down_cost {
+                        (*right_cost, right_path.clone())
+                    } else {
+                        (*down_cost, down_path.clone())
+                    }
+                }
+                (Some((right_x, right_y)), None) => {
+                    let (right_cost, right_path) = memo.get(&(right_x, right_y)).unwrap();
+                    (*right_cost, right_path.clone())
+                }
+                (None, Some((down_x, down_y))) => {
+                    let (down_cost, down_path) = memo.get(&(down_x, down_y)).unwrap();
+                    (*down_cost, down_path.clone())
+                }
+            };
+            let mut self_path = min_next_path;
+            self_path.push((x, y));
+            if (x, y) == (0, 0) {
+                return self_path;
+            }
+            memo.insert((x, y), (self_cost + min_next_cost, self_path));
+        }
+        // By reducing the value of lower_left_x or lower_left_y, we're moving on to
+        // the next "/"-shaped slice (one spot to the left our most recent slice).
+        match lower_left_x.checked_sub(1) {
+            Some(new_x) => lower_left_x = new_x,
+            None => lower_left_y -= 1,
+        }
+    }
+}
 
 pub fn minimal_cost_top_down(grid: &[&[u32]]) -> Vec<Coords> {
     let mut memo: HashMap<Coords, (u32, Vec<Coords>)> = HashMap::new();
@@ -79,6 +138,7 @@ mod tests {
             (4, 4),
         ];
         assert_eq!(minimal_cost_top_down(test_grid), expected_solution);
+        assert_eq!(minimal_cost_bottom_up(test_grid), expected_solution);
     }
 
     #[test]
@@ -109,5 +169,6 @@ mod tests {
             (6, 6),
         ];
         assert_eq!(minimal_cost_top_down(test_grid), expected_solution);
+        assert_eq!(minimal_cost_bottom_up(test_grid), expected_solution);
     }
 }
