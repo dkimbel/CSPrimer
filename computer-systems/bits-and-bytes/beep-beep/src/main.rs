f9f50e0bf2849646e081a131390cfe49b217ccbb
Daniel Kimbel <dk@danielkimbel.com>
2025-02-04 22:58:28 -0500

    Get full beep program working (except we're capped at five beeps)

--- computer-systems/bits-and-bytes/beep-beep/src/main.rs
+++ computer-systems/bits-and-bytes/beep-beep/src/main.rs
@@ -1,8 +1,11 @@
+use crossterm::terminal::enable_raw_mode;
 use std::io::{self, Read, Write};
 
 const BELL_CHAR: u8 = 0x7;
 
 fn main() {
+    enable_raw_mode().expect("Failed to enable raw mode");
+
     let mut buffer: [u8; 1] = [0; 1];
 
     loop {
@@ -10,7 +13,13 @@ fn main() {
             .read(&mut buffer)
             .expect("Failed to read from stdin");
 
+        if buffer[0] == 0x3 {
+            // handle ctrl-c (at least on macOS)
+            return;
+        }
         if let Some(digit) = (buffer[0] as char).to_digit(10) {
+            // macOS is only willing to play five bells at a time; we'll sometimes
+            // try to send as many as nine, but only five will play
             let bells = vec![BELL_CHAR; digit as usize];
             io::stdout()
                 .write_all(&bells)
