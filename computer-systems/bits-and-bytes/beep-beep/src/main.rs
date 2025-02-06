b48ca3308d0706e4ed700a0db9a10b0105653f9d
Daniel Kimbel <dk@danielkimbel.com>
2025-02-05 22:47:44 -0500

    Be careful to leave the terminal in a good state, using termios to restore its attributes

--- computer-systems/bits-and-bytes/beep-beep/src/main.rs
+++ computer-systems/bits-and-bytes/beep-beep/src/main.rs
@@ -1,30 +1,41 @@
 use crossterm::terminal::enable_raw_mode;
 use std::io::{self, Read, Write};
+use termios::{tcsetattr, Termios};
 
 const BELL_CHAR: u8 = 0x7;
 
 fn main() {
+    let terminal_attrs = Termios::from_fd(0).expect("Failed to initialize terminal attrs");
+
     enable_raw_mode().expect("Failed to enable raw mode");
 
     let mut buffer: [u8; 1] = [0; 1];
 
     loop {
-        io::stdin()
-            .read(&mut buffer)
-            .expect("Failed to read from stdin");
+        if let Err(e) = io::stdin().read(&mut buffer) {
+            tcsetattr(0, 0, &terminal_attrs).expect("Failed to restore terminal attrs");
+            eprintln!("Failed to read from stdin: {e}");
+            std::process::exit(1);
+        }
 
         if buffer[0] == 0x3 {
             // handle ctrl-c (at least on macOS)
-            return;
+            tcsetattr(0, 0, &terminal_attrs).expect("Failed to restore terminal attrs");
+            std::process::exit(0);
         }
+
         if let Some(digit) = (buffer[0] as char).to_digit(10) {
             // macOS is only willing to play five bells at a time; we'll sometimes
             // try to send as many as nine, but only five will play
             let bells = vec![BELL_CHAR; digit as usize];
-            io::stdout()
+            if let Err(e) = io::stdout()
                 .write_all(&bells)
                 .and_then(|_| io::stdout().flush())
-                .expect("Failed to write or flush stdout");
+            {
+                tcsetattr(0, 0, &terminal_attrs).expect("Failed to restore terminal attrs");
+                eprintln!("Failed to write or flush to stdout: {e}");
+                std::process::exit(1);
+            }
         }
     }
 }
